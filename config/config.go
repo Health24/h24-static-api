ebff2c432b64d550b1fd85d27fd7071db8f3170e
vs <vshaveyko@gmail.com>
2017-07-26 15:43:56 +0300

    fixed db url

--- config/config.go
+++ config/config.go
@@ -12,6 +12,8 @@ var AppSecret = os.Getenv("APP_SECRET")
 var DbName = os.Getenv("POSTGRES_DB")
 var DbUser = os.Getenv("POSTGRES_USER")
 
-var DATABASE_URL = fmt.Sprintf("postgresql://%s@localhost:5432/%s?connect_timeout=5&sslmode=disable",
-	"health24",
-	"health24_production")
+// var DATABASE_URL = fmt.Sprintf("postgresql://%s@localhost:5432/%s?connect_timeout=5&sslmode=disable",
+//   DbUser,
+//   DbName)
+
+var DATABASE_URL = "postgresql://health24@localhost:5432/health24_production?connect_timeout=5&sslmode=disable"
